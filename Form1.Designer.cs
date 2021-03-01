277c62306f2d7d2db71e59f052f35adb91c69adf
Alex Foshay <FoshAlex157@GoTVDSB.ca>
2021-03-01 13:00:24 -0500

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,151 @@
+﻿namespace Part_4___Random_Numbers
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblRan = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtMin = new System.Windows.Forms.TextBox();
+            this.txtMax = new System.Windows.Forms.TextBox();
+            this.btnInt = new System.Windows.Forms.Button();
+            this.btnDouble = new System.Windows.Forms.Button();
+            this.lblRandom = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblRan
+            // 
+            this.lblRan.AutoSize = true;
+            this.lblRan.Font = new System.Drawing.Font("Sitka Text", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblRan.Location = new System.Drawing.Point(26, 13);
+            this.lblRan.Name = "lblRan";
+            this.lblRan.Size = new System.Drawing.Size(280, 21);
+            this.lblRan.TabIndex = 0;
+            this.lblRan.Text = "Enter a Minimum and Maximum Value";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Sitka Text", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(35, 50);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(83, 21);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Minimum:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Sitka Text", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(35, 90);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(85, 21);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Maximum:";
+            // 
+            // txtMin
+            // 
+            this.txtMin.Font = new System.Drawing.Font("Sitka Text", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtMin.Location = new System.Drawing.Point(125, 47);
+            this.txtMin.Name = "txtMin";
+            this.txtMin.Size = new System.Drawing.Size(234, 26);
+            this.txtMin.TabIndex = 3;
+            // 
+            // txtMax
+            // 
+            this.txtMax.Font = new System.Drawing.Font("Sitka Text", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtMax.Location = new System.Drawing.Point(125, 90);
+            this.txtMax.Name = "txtMax";
+            this.txtMax.Size = new System.Drawing.Size(234, 26);
+            this.txtMax.TabIndex = 4;
+            // 
+            // btnInt
+            // 
+            this.btnInt.Font = new System.Drawing.Font("Sitka Text", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnInt.Location = new System.Drawing.Point(65, 148);
+            this.btnInt.Name = "btnInt";
+            this.btnInt.Size = new System.Drawing.Size(112, 35);
+            this.btnInt.TabIndex = 5;
+            this.btnInt.Text = "Get Integer";
+            this.btnInt.UseVisualStyleBackColor = true;
+            this.btnInt.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnDouble
+            // 
+            this.btnDouble.Font = new System.Drawing.Font("Sitka Text", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDouble.Location = new System.Drawing.Point(240, 148);
+            this.btnDouble.Name = "btnDouble";
+            this.btnDouble.Size = new System.Drawing.Size(119, 35);
+            this.btnDouble.TabIndex = 6;
+            this.btnDouble.Text = "Get Double";
+            this.btnDouble.UseVisualStyleBackColor = true;
+            this.btnDouble.Click += new System.EventHandler(this.btnDouble_Click);
+            // 
+            // lblRandom
+            // 
+            this.lblRandom.AutoSize = true;
+            this.lblRandom.Font = new System.Drawing.Font("Sitka Text", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblRandom.Location = new System.Drawing.Point(193, 209);
+            this.lblRandom.Name = "lblRandom";
+            this.lblRandom.Size = new System.Drawing.Size(34, 21);
+            this.lblRandom.TabIndex = 7;
+            this.lblRandom.Text = "0.0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(441, 283);
+            this.Controls.Add(this.lblRandom);
+            this.Controls.Add(this.btnDouble);
+            this.Controls.Add(this.btnInt);
+            this.Controls.Add(this.txtMax);
+            this.Controls.Add(this.txtMin);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblRan);
+            this.Name = "Form1";
+            this.Text = "Random Numbers";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblRan;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtMin;
+        private System.Windows.Forms.TextBox txtMax;
+        private System.Windows.Forms.Button btnInt;
+        private System.Windows.Forms.Button btnDouble;
+        private System.Windows.Forms.Label lblRandom;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Part_4___Random_Numbers
+{
+    public partial class Form1 : Form
+    {
+
+        Random generator = new Random();
+
+        string ranF;
+        int maxInt;
+        int minInt;
+        int ran;
+        double dMax;
+        double dMin;
+        double ranD;
+
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            //Int
+
+                maxInt = Convert.ToInt32(txtMax.Text);
+                minInt = Convert.ToInt32(txtMin.Text);
+
+
+
+                ran = generator.Next(minInt, maxInt+1);
+                ranF = Convert.ToString(ran);
+                lblRandom.Text = $"Integer: {ranF}";
+
+
+
+        }
+
+
+        private void btnDouble_Click(object sender, EventArgs e)
+        {
+            //Double
+
+            dMax = Convert.ToDouble(txtMax.Text);
+            dMin = Convert.ToDouble(txtMin.Text);
+
+            ranD = generator.NextDouble() * (dMax - dMin) + dMin;
+            ranF = Convert.ToString(ranD);
+            lblRandom.Text = $"Double: {ranF}";
+        }
+    }
+}
